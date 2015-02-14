436145b12e7610ae1c7d440abb5b092d7d93173f
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-02-14 11:15:43 +0000

    Added helper function to check for supported API calls

--- inc/app.php
+++ inc/app.php
@@ -39,6 +39,49 @@ class NxFitbit
     }
 
     /**
+     * Helper function to check for supported API calls
+     * @param null $key
+     * @return array|null|string
+     */
+    public function supportedApi($key = NULL) {
+        $database_array = array(
+            'all'                  => 'Everything',
+            'floors'               => 'Floors Climed',
+            'foods'                => 'Calorie Intake',
+            'badges'               => 'Badges',
+            'sleep'                => 'Sleep Records',
+            'body'                 => 'Weight & Body Fat Records',
+            'goals'                => 'Personal Goals',
+            'water'                => 'Water Intake',
+            'activities'           => 'Pedomitor & Activities',
+            'leaderboard'          => 'Friends',
+            'devices'              => 'Device Status',
+            'caloriesOut'          => 'Calories Out',
+            'goals_calories'       => 'Calorie Goals',
+            'minutesVeryActive'    => 'Minutes Very Active',
+            'minutesFairlyActive'  => 'Minutes Fairly Active',
+            'minutesLightlyActive' => 'Minutes Lightly Active',
+            'minutesSedentary'     => 'Minutes Sedentary',
+            'elevation'            => 'Elevation',
+            'distance'             => 'Distance Traveled',
+            'pedomitor'            => 'Steps Taken',
+            'profile'              => 'User Profile',
+            'heart'                => 'Heart Rates',
+        );
+        asort($database_array);
+
+        if (is_null($key)) {
+            return $database_array;
+        } else {
+            if (array_key_exists($key, $database_array)) {
+                return $database_array[$key];
+            } else {
+                return $key;
+            }
+        }
+    }
+
+    /**
      * Get list of pending cron jobs from database
      * @return array|bool
      */
