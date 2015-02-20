a2e4a42fd767c943cc32051a0e993106afac11ec
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-02-20 18:56:46 +0000

    Improved error messages

--- cron.php
+++ cron.php
@@ -92,6 +92,8 @@
                             }
                         }
                     }
+                } else {
+                    nxr("There is nothing to queue");
                 }
             }
 
