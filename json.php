25a1c060cfa18ac46ed1d40dcfa646c7b158b427
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-02-26 21:20:15 +0000

    Added JSON cache files

--- json.php
+++ json.php
@@ -4,15 +4,34 @@
     header('Content-type: application/json');
 
     if (array_key_exists("user", $_GET) && array_key_exists("data", $_GET)) {
-        require_once(dirname(__FILE__) . "/inc/dataReturn.php");
-        $dataReturnClass = new dataReturn($_GET['user']);
-        if ($dataReturnClass->isUser()) {
-            echo json_encode($dataReturnClass->returnUserRecords($_GET));
-            //echo "<pre>";
-            //print_r($dataReturnClass->returnUserRecords($_GET));
-            //echo "</pre>";
+        if (is_writable('cache')) {
+            // cache files are created like cache/abcdef123456...
+            $cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($_SERVER['REQUEST_URI']);
+
+            if (file_exists($cacheFile)) {
+                $fh = fopen($cacheFile, 'r');
+                $cacheTime = trim(fgets($fh));
+
+                // if data was cached recently, return cached data
+                if ($cacheTime > strtotime('-15 minutes')) {
+                    echo fread($fh, filesize($cacheFile));
+                    return true;
+                }
+
+                // else delete cache file
+                fclose($fh);
+                unlink($cacheFile);
+            }
+
+            $json = query_api();
+            echo $json;
+
+            $fh = fopen($cacheFile, 'w');
+            fwrite($fh, time() . "\n");
+            fwrite($fh, $json);
+            fclose($fh);
         } else {
-            echo json_error(101);
+            echo query_api();
         }
     } elseif (!array_key_exists("user", $_GET)) {
         echo json_error(100);
@@ -20,6 +39,20 @@
         echo json_error(102);
     }
 
+    function query_api() {
+        require_once(dirname(__FILE__) . "/inc/dataReturn.php");
+        $dataReturnClass = new dataReturn($_GET['user']);
+        if ($dataReturnClass->isUser()) {
+            $json = json_encode($dataReturnClass->returnUserRecords($_GET));
+            //echo "<pre>";
+            //print_r($dataReturnClass->returnUserRecords($_GET));
+            //echo "</pre>";
+            return $json;
+        } else {
+            echo json_error(101);
+            return "";
+        }
+    }
 
     function json_error($errNumber) {
         $errMessage = "";
