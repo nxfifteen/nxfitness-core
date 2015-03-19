7677781bf5df7265a6b272bf3e2ff573f2bc2c61
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-03-19 09:35:16 +0000

    Updated test file

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    $test = array("user" => "269VLG", "data" => "FoodDiary");
+    $test = array("user" => "269VLG", "data" => "Sleep");
 
     require_once(dirname(__FILE__) . "/inc/dataReturn.php");
     $dataReturnClass = new dataReturn($test['user']);
