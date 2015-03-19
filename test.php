213e899f09394b98ce4a9dfa355b66fa64c2761c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-03-19 16:48:20 +0000

    Updated test file

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
-    $test = array("user" => "269VLG", "data" => "StepGoal");
-
     require_once(dirname(__FILE__) . "/inc/dataReturn.php");
+
+    $test = array("user" => "269VLG", "data" => "Tracked", "period" => "last14");
     $dataReturnClass = new dataReturn($test['user']);
     print_r($dataReturnClass->returnUserRecords($test));
+
+    //$test = array("user" => "269VLG", "data" => "StepsGoal");
+    //print_r($dataReturnClass->returnUserRecords($test));
