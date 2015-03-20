fddd5a98a03263bf4b973abece9035f8aa3c1575
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-03-20 10:50:23 +0000

    Added map of cache names to data sets for issue #35

--- inc/config.php
+++ inc/config.php
@@ -37,8 +37,71 @@
         public function getRelatedCacheNames($activityName) {
             $cacheNames = array();
             switch ($activityName) {
+                case "activities":
+                    $cacheNames = array('activity', 'dashboard', 'weekpedometer', 'aboutme', 'keypoints', 'steps', 'tracked');
+                    break;
+                case "activity_log":
+                    $cacheNames = array('activityhistory');
+                    break;
+                case "badges":
+                    $cacheNames = array('topbadges');
+                    break;
+                case "body":
+                    $cacheNames = array('trend', 'weight');
+                    break;
+                case "caloriesOut":
+                    $cacheNames = array();
+                    break;
+                case "devices":
+                    $cacheNames = array('devices');
+                    break;
+                case "distance":
+                    $cacheNames = array('dashboard', 'weekpedometer', 'aboutme', 'keypoints', 'steps', 'tracked');
+                    break;
+                case "elevation":
+                    $cacheNames = array();
+                    break;
+                case "floors":
+                    $cacheNames = array('dashboard', 'weekpedometer', 'aboutme', 'keypoints', 'steps', 'tracked');
+                    break;
+                case "foods":
+                    $cacheNames = array('food', 'fooddiary');
+                    break;
                 case "goals":
-                    $cacheNames = array('tracked');
+                    $cacheNames = array('dashboard', 'tracked', 'steps');
+                    break;
+                case "goals_calories":
+                    $cacheNames = array('trend');
+                    break;
+                case "heart":
+                    $cacheNames = array();
+                    break;
+                case "leaderboard":
+                    $cacheNames = array('trend');
+                    break;
+                case "minutesFairlyActive":
+                    $cacheNames = array('activity');
+                    break;
+                case "minutesLightlyActive":
+                    $cacheNames = array('activity');
+                    break;
+                case "minutesSedentary":
+                    $cacheNames = array('activity');
+                    break;
+                case "minutesVeryActive":
+                    $cacheNames = array('activity');
+                    break;
+                case "profile":
+                    $cacheNames = array('trend');
+                    break;
+                case "sleep":
+                    $cacheNames = array('sleep');
+                    break;
+                case "steps":
+                    $cacheNames = array('dashboard', 'weekpedometer', 'aboutme', 'keypoints', 'steps', 'tracked');
+                    break;
+                case "water":
+                    $cacheNames = array('water');
                     break;
                 default:
                     nxr("Unknown cache file for $activityName");
