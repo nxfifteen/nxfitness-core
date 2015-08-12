05b4776abbeffc7c189f58f91161eea78263f6b4
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2015-08-12 21:38:13 +0100

    Issue #1: Increased size of integers from 32bit 11 to 64bit 20

--- logid_to_64.sql
+++ logid_to_64.sql
@@ -1,5 +1,5 @@
 -- All numerical ids in the Fitbit API to be unsigned 64 bit integers
-ALTER TABLE `nx_fitbit_activity_log` CHANGE `logId` `logId` BIGINT(11) NOT NULL, CHANGE `activityId` `activityId` BIGINT(11) NOT NULL, CHANGE `activityParentId` `activityParentId` BIGINT(11) NOT NULL;
-ALTER TABLE `nx_fitbit_lnk_sleep2usr` CHANGE `sleeplog` `sleeplog` BIGINT(11) NOT NULL;
-ALTER TABLE `nx_fitbit_logSleep` CHANGE `logId` `logId` BIGINT(11) NOT NULL;
-ALTER TABLE `nx_fitbit_water` CHANGE `id` `id` BIGINT(11) NOT NULL;
+ALTER TABLE `nx_fitbit_activity_log` CHANGE `logId` `logId` BIGINT(20) NOT NULL, CHANGE `activityId` `activityId` BIGINT(20) NOT NULL, CHANGE `activityParentId` `activityParentId` BIGINT(20) NOT NULL;
+ALTER TABLE `nx_fitbit_lnk_sleep2usr` CHANGE `sleeplog` `sleeplog` BIGINT(20) NOT NULL;
+ALTER TABLE `nx_fitbit_logSleep` CHANGE `logId` `logId` BIGINT(20) NOT NULL;
+ALTER TABLE `nx_fitbit_water` CHANGE `id` `id` BIGINT(20) NOT NULL;
