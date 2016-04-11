594078ebe64610dde758470211998782dc8ea187
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2016-04-11 17:46:10 +0100

    Removed command line output from index pages

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ if (!function_exists("nxr")) {
             fclose($fh);
         }
 
-        if (php_sapi_name() == "cli") {
-            echo date("Y-m-d H:i:s") . ": " . $msg . "\n";
-        }
+//        if (php_sapi_name() == "cli") {
+//            echo date("Y-m-d H:i:s") . ": " . $msg . "\n";
+//        }
     }
 }
 
