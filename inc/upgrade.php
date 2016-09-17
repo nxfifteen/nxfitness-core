344651833439e5415ea11068ff0895f084bb4471
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2016-09-17 18:36:38 +0100

    Issue #43 - Added update function to support new database layout requirments

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -5,16 +5,6 @@
      */
     class Upgrade {
 
-        /**
-         * @var NxFitbit
-         */
-        protected $AppClass;
-
-	    /**
-	     * @var String
-	     */
-	    protected $UserID;
-
 	    /**
 	     * @var integer
 	     */
@@ -28,7 +18,7 @@
 	    /**
 	     * @var String
 	     */
-	    protected $VersionInstalling = "0.0.0.2";
+	    protected $VersionInstalling = "0.0.0.4";
 
 	    /**
 	     * @var String
@@ -39,29 +29,105 @@
 	     * @var array
 	     */
 	    protected $VersionCurrentArray;
+	    
+	    /**
+	     * @var medoo
+	     */
+	    protected $database;
+
+	    /**
+	     * @var fitbit
+	     */
+	    protected $fitbitapi;
+
+	    /**
+	     * @var config
+	     */
+	    protected $settings;
 
         /**
          * Upgrade constructor.
          *
          * @param $userFid
          */
-        public function __construct($userFid) {
-	        $this->UserID = $userFid;
+        public function __construct() {
+	        require_once(dirname(__FILE__) . "/config.php");
+	        $this->setSettings(new config());
 
-		    require_once(dirname(__FILE__) . "/app.php");
-	        $this->AppClass = new NxFitbit();
+	        require_once(dirname(__FILE__) . "/../library/medoo.php");
+	        $this->setDatabase(new medoo(array(
+		        'database_type' => 'mysql',
+		        'database_name' => $this->getSetting("db_name"),
+		        'server'        => $this->getSetting("db_server"),
+		        'username'      => $this->getSetting("db_username"),
+		        'password'      => $this->getSetting("db_password"),
+		        'charset'       => 'utf8'
+	        )));
 
-	        if ($this->AppClass->isUser($this->UserID)) {
-		        echo " - Valid User\n";
-	        } else {
-		        echo " - " . $userFid . " is not valid\n";
-	        }
-	        $this->AppClass->setSetting("version", "0.0.0.1", TRUE);
-
+	        $this->getSettings()->setDatabase($this->getDatabase());
         }
 
-	    private function getAppClass() {
-	    	return $this->AppClass;
+	    /**
+	     * @param config $settings
+	     */
+	    private function setSettings($settings) {
+		    $this->settings = $settings;
+	    }
+
+	    /**
+	     * @return config
+	     */
+	    public function getSettings() {
+		    return $this->settings;
+	    }
+
+	    /**
+	     * @param medoo $database
+	     */
+	    private function setDatabase($database) {
+		    $this->database = $database;
+	    }
+
+	    /**
+	     * @return medoo
+	     */
+	    public function getDatabase() {
+		    return $this->database;
+	    }
+
+	    /**
+	     * Get settings from config class
+	     *
+	     * @param                $key
+	     * @param null           $default
+	     * @param bool           $query_db
+	     *
+	     * @return string
+	     */
+	    public function getSetting($key, $default = NULL, $query_db = TRUE) {
+		    return $this->getSettings()->get($key, $default, $query_db);
+	    }
+
+	    /**
+	     * Set value in database/config class
+	     *
+	     * @param           $key
+	     * @param           $value
+	     * @param bool   $query_db
+	     *
+	     * @return bool
+	     */
+	    public function setSetting($key, $value, $query_db = TRUE) {
+		    return $this->getSettings()->set($key, $value, $query_db);
+	    }
+
+	    private function wasMySQLError( $error ) {
+	    	if (is_null($error[2])) {
+	    		return false;
+		    } else {
+			    print_r($error);
+		    	return true;
+		    }
 	    }
 
 	    public function getInstallingVersion() {
@@ -74,7 +140,7 @@
 
 	    public function getInstallVersion() {
 		    if (is_null($this->VersionCurrent)) {
-			    $this->VersionCurrent = $this->getAppClass()->getSetting("version", "0.0.0.1", TRUE);
+			    $this->VersionCurrent = $this->getSetting("version", "0.0.0.1", TRUE);
 
 			    $this->VersionCurrentArray = explode(".", $this->VersionCurrent);
 		    }
@@ -134,37 +200,153 @@
 		    foreach ( $this->UpdateFunctions as $updateFunction ) {
 			    if (method_exists($this, $updateFunction)) {
 				    echo " Running " . $updateFunction . "\n";
-				    $this->$updateFunction();
+				    if ($this->$updateFunction()) {
+					    echo "  + " . $updateFunction . " [OKAY]\n";
+				    } else {
+					    echo "  + " . $updateFunction . " [FAILED]\n";
+					    return false;
+				    }
 			    }
 		    }
 
-		    //$this->getAppClass()->setSetting("version", $this->VersionInstalling, TRUE);
+		    //$this->setSetting("version", $this->VersionInstalling, TRUE);
 	    }
-
-	    ///** @noinspection PhpUnusedPrivateMethodInspection */
-	    //private function update_2() {
-	    //	$this->getAppClass()->setSetting("version", "0.0.0.2", TRUE);
-	    //
-	    //}
 
 	    /** @noinspection PhpUnusedPrivateMethodInspection */
 	    private function update_2() {
-	    	
+		    $db_prefix = $this->getSetting("db_prefix", FALSE);
 
-	    	$this->getAppClass()->setSetting("version", "0.0.0.2", TRUE);
+		    $this->getDatabase()->query("CREATE TABLE `".$db_prefix."streak_goal` (  `uid` int(6) NOT NULL,  `fuid` varchar(8) NOT NULL,  `goal` varchar(255) NOT NULL,  `start_date` date NOT NULL,  `end_date` date NOT NULL,  `length` int(3) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+		    $this->getDatabase()->query("ALTER TABLE `".$db_prefix."streak_goal`  ADD PRIMARY KEY (`fuid`,`goal`,`start_date`) USING BTREE,  ADD UNIQUE KEY `uid` (`uid`);");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+		    $this->getDatabase()->query("ALTER TABLE `".$db_prefix."streak_goal` MODIFY `uid` int(6) NOT NULL AUTO_INCREMENT;");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+		    $this->getDatabase()->query("ALTER TABLE `".$db_prefix."streak_goal` ADD CONSTRAINT `".$db_prefix."streak_goal_ibfk` FOREIGN KEY (`fuid`) REFERENCES `".$db_prefix."users` (`fuid`) ON DELETE NO ACTION;");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+		    $this->setSetting("version", "0.0.0.2", TRUE);
+		    return true;
 	    }
 
-	    /*
-CREATE TABLE `nx_fitbit_streak_goal` (
-  `fuid` varchar(8) NOT NULL,
-  `goal` varchar(255) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	    /** @noinspection PhpUnusedPrivateMethodInspection */
+	    private function update_3() {
+		    $db_prefix = $this->getSetting("db_prefix", FALSE);
 
+		    $this->getDatabase()->query("ALTER TABLE `".$db_prefix."streak_goal` ADD FOREIGN KEY (`fuid`) REFERENCES `".$db_prefix."users`(`fuid`) ON DELETE NO ACTION ON UPDATE RESTRICT; ");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
 
-ALTER TABLE `nx_fitbit_streak_goal`
-  ADD PRIMARY KEY (`fuid`);
-		 */
+		    $this->setSetting("version", "0.0.0.3", TRUE);
+		    return true;
+	    }
+
+	    /** @noinspection PhpUnusedPrivateMethodInspection */
+	    private function update_4() {
+		    $db_prefix = $this->getSetting("db_prefix", FALSE);
+
+		    $users = $this->getDatabase()->select($db_prefix . "users", "fuid");
+		    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+		    foreach ( $users as $user ) {
+			    echo "  " . $user . "\n";
+			    $steps = $this->getDatabase()->select($db_prefix . "steps", array(
+				    "[>]" . $db_prefix . "steps_goals" => array(
+					    "user",
+					    "date"
+				    ) ), array(
+				    $db_prefix . "steps.date",
+				    $db_prefix . "steps.steps",
+				    $db_prefix . "steps_goals.steps(step_goal)"
+			    ), array(
+				    $db_prefix . "steps.user" => $user,
+				    "ORDER" => $db_prefix . "steps.date ASC"
+			    ));
+			    if ($this->wasMySQLError($this->getDatabase()->error())) return false;
+
+			    $streak = false;
+			    $streak_start = "";
+			    $streak_prevdate = "";
+			    $streak_end = "";
+			    foreach ( $steps as $step ) {
+				    echo "   - " . $step['date'] . " " . $step['steps'] . "/" . $step['step_goal'];
+
+			    	if ( $step['steps'] >= $step['step_goal'] ) {
+					    if ( !$streak ) {
+						    $streak = true;
+						    $streak_start = $step['date'];
+
+						    echo "\tnew streak started";
+					    }
+					    $streak_prevdate = $step['date'];
+				    } else {
+					    if ( $streak ) {
+						    $streak = false;
+						    $streak_end = $streak_prevdate;
+
+						    $date1 = new DateTime($streak_end);
+						    $date2 = new DateTime($streak_start);
+
+						    $days_between = $date2->diff($date1)->format("%a");
+						    $days_between = $days_between + 1;
+
+						    echo "\tnew streak broken. " . $streak_start . " to " . $streak_end . " (".$days_between.")";
+
+						    if ($days_between > 1) {
+							    if ( $this->getDatabase()->has( $db_prefix . "streak_goal", array(
+								    "AND" => array(
+									    "fuid"       => $user,
+									    "goal"       => "steps",
+									    "start_date" => $streak_start
+								    )
+							    ) )
+							    ) {
+								    $this->getDatabase()->update( $db_prefix . "streak_goal", array(
+									    "end_date" => $streak_end,
+									    "length"   => $days_between
+								    ),
+									    array(
+										    "AND" => array(
+											    "fuid"       => $user,
+											    "goal"       => "steps",
+											    "start_date" => $streak_start
+										    )
+									    )
+								    );
+								    if ( $this->wasMySQLError( $this->getDatabase()->error() ) ) {
+									    print_r( $this->getDatabase()->log() );
+
+									    return FALSE;
+								    }
+							    } else {
+								    $this->getDatabase()->insert( $db_prefix . "streak_goal", array(
+									    "fuid"       => $user,
+									    "goal"       => "steps",
+									    "start_date" => $streak_start,
+									    "end_date"   => $streak_end,
+									    "length"     => $days_between
+								    ) );
+								    if ( $this->wasMySQLError( $this->getDatabase()->error() ) ) {
+									    print_r( $this->getDatabase()->log() );
+
+									    return FALSE;
+								    }
+							    }
+						    }
+					    }
+				    }
+
+				    echo "\n";
+			    }
+
+		    }
+
+		    $this->setSetting("version", "0.0.0.4", TRUE);
+		    return true;
+
+	    }
+
 
     }
