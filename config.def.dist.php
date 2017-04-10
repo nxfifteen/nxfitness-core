b9e2a9168d46b9dd3e3b4767f0a075de6d738294
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-10 20:31:14 +0100

    fix(error_reporting): Added user option to sentry logging
    Added check for SENTRY_DSN definition. If not defined will not report errors to sentry.
    
    Closes #66

--- config.def.dist.php
+++ config.def.dist.php
@@ -1,5 +1,6 @@
 <?php
 	define( 'APP_VERSION', '0.0.0.8' );
+	//define( 'SENTRY_DSN', 'https://764e9491e75c4438ac0608d9ad2fd17a:62d9b7d807b0422e803bfe75e0d70998@sentry.io/156527' );
 	if ( file_exists( dirname( __FILE__ ) . "/config.def.php" ) ) {
 		require_once( dirname( __FILE__ ) . "/config.def.php" );
 	}
