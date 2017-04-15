9b8bed279b8dee7bccfcddfe4dc739d28b39b54c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-15 08:24:34 +0100

    feat(user_ux): Basic user items to user menu
    Added basic notification count and user id settings to user menu
    
    Issue #73
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ux/inc/_html.topUserMenu.php
+++ ux/inc/_html.topUserMenu.php
@@ -1,18 +1,36 @@
+<?php
+	$userProfile = $App->getUserProfile();
+
+	if (array_key_exists("HeaderNotificationBar", $_SESSION) && is_array($_SESSION['HeaderNotificationBar']) && count($_SESSION['HeaderNotificationBar']) > 0) {
+		$HeaderNotificationBar = $_SESSION['HeaderNotificationBar'];
+
+		nxr(" * HeaderNotificationBar taken from session");
+	} else {
+
+		$HeaderNotificationBar = array();
+		if (!$App->getNxFit()->isUserOAuthAuthorised($_COOKIE['_nx_fb_usr'])) {
+			array_push($HeaderNotificationBar, $App->getThemeWidgets("HeaderNotificationBar", array("msg" => "<a href=\"../authorise\">Fitbit OAuth Setup Required</a>", "urgency" => "danger", "icon" => "bug")));
+		} else {
+			if (isset($userProfile['cooldown']) && strtotime($userProfile['cooldown']) > strtotime(date("Y-m-d H:i:s"))) {
+				array_push($HeaderNotificationBar, $App->getThemeWidgets("HeaderNotificationBar", array("msg" => "Sync Status - Fitbit API limit reached. ", "urgency" => "danger", "icon" => "dashboard")));
+			}
+
+			if (count($HeaderNotificationBar) > 0) $_SESSION['HeaderNotificationBar'] = $HeaderNotificationBar;
+		}
+
+		nxr(" * HeaderNotificationBar set in session");
+	}
+?>
+
 <ul class="nav navbar-nav ml-auto">
 
                 <li class="nav-item d-md-down-none">
-                    <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
-                </li>
-                <li class="nav-item d-md-down-none">
-                    <a class="nav-link" href="#"><i class="icon-list"></i></a>
-                </li>
-                <li class="nav-item d-md-down-none">
-                    <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
+                    <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger"><?php echo count($HeaderNotificationBar); ?></span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                        <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-                        <span class="d-md-down-none">admin</span>
+                        <img src="<?php echo $userProfile['avatar']; ?>" class="img-avatar" alt="<?php echo $userProfile['name']; ?>">
+                        <span class="d-md-down-none"><?php echo $userProfile['name']; ?></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
 
@@ -20,10 +38,7 @@
                             <strong>Account</strong>
                         </div>
 
-                        <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> Updates<span class="badge badge-info">42</span></a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> Messages<span class="badge badge-success">42</span></a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-tasks"></i> Tasks<span class="badge badge-danger">42</span></a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-comments"></i> Comments<span class="badge badge-warning">42</span></a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> Updates<span class="badge badge-info"><?php echo count($HeaderNotificationBar); ?></span></a>
 
                         <div class="dropdown-header text-center">
                             <strong>Settings</strong>
@@ -31,11 +46,8 @@
 
                         <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Profile</a>
                         <a class="dropdown-item" href="#"><i class="fa fa-wrench"></i> Settings</a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-usd"></i> Payments<span class="badge badge-default">42</span></a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-file"></i> Projects<span class="badge badge-primary">42</span></a>
                         <div class="divider"></div>
-                        <a class="dropdown-item" href="#"><i class="fa fa-shield"></i> Lock Account</a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Logout</a>
+                        <a class="dropdown-item" href="logout"><i class="fa fa-lock"></i> Logout</a>
                     </div>
                 </li>
                 <li class="nav-item d-md-down-none">
