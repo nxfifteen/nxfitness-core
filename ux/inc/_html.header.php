475788549cb129dfea14e3c15682813a98d75bf7
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-15 07:25:59 +0100

    feat(user_ux): Corrected inclusion indents
    Issue #73
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ux/inc/_html.header.php
+++ ux/inc/_html.header.php
@@ -1,31 +1,31 @@
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="NxFIFTEEN || NxFITNESS">
-<meta name="author" content="Stuart McCulloch Anderson">
-<meta name="keyword" content="">
-<link rel="shortcut icon" href="img/favicon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="NxFIFTEEN || NxFITNESS">
+        <meta name="author" content="Stuart McCulloch Anderson">
+        <meta name="keyword" content="">
+        <link rel="shortcut icon" href="img/favicon.png">
 
-<title>NxFIFTEEN Fitness & Health</title>
+        <title>NxFIFTEEN Fitness & Health</title>
 
-<!-- Icons -->
-<!--<link href="css/nomie.css" rel="stylesheet">-->
-<link href="css/font-awesome.min.css" rel="stylesheet">
-<link href="css/simple-line-icons.css" rel="stylesheet">
+        <!-- Icons -->
+        <!--<link href="css/nomie.css" rel="stylesheet">-->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/simple-line-icons.css" rel="stylesheet">
 
-<!-- Main styles for this application -->
-<link href="css/style.css" rel="stylesheet">
+        <!-- Main styles for this application -->
+        <link href="css/style.css" rel="stylesheet">
 
-<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png?v=A00aBgaPR5">
-<link rel="icon" type="image/png" href="img/favicon-32x32.png?v=A00aBgaPR5" sizes="32x32">
-<link rel="icon" type="image/png" href="img/favicon-194x194.png?v=A00aBgaPR5" sizes="194x194">
-<link rel="icon" type="image/png" href="img/android-chrome-192x192.png?v=A00aBgaPR5" sizes="192x192">
-<link rel="icon" type="image/png" href="img/favicon-16x16.png?v=A00aBgaPR5" sizes="16x16">
-<link rel="manifest" href="img/manifest.json?v=A00aBgaPR5">
-<link rel="mask-icon" href="img/safari-pinned-tab.svg?v=A00aBgaPR5" color="#b91d47">
-<link rel="shortcut icon" href="img/favicon.ico?v=A00aBgaPR6">
-<meta name="apple-mobile-web-app-title" content="NxFITNESS">
-<meta name="application-name" content="NxFITNESS">
-<meta name="msapplication-TileColor" content="#db1d1d">
-<meta name="msapplication-TileImage" content="img/mstile-144x144.png?v=A00aBgaPR5">
-<meta name="theme-color" content="#ffffff">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png?v=A00aBgaPR5">
+        <link rel="icon" type="image/png" href="img/favicon-32x32.png?v=A00aBgaPR5" sizes="32x32">
+        <link rel="icon" type="image/png" href="img/favicon-194x194.png?v=A00aBgaPR5" sizes="194x194">
+        <link rel="icon" type="image/png" href="img/android-chrome-192x192.png?v=A00aBgaPR5" sizes="192x192">
+        <link rel="icon" type="image/png" href="img/favicon-16x16.png?v=A00aBgaPR5" sizes="16x16">
+        <link rel="manifest" href="img/manifest.json?v=A00aBgaPR5">
+        <link rel="mask-icon" href="img/safari-pinned-tab.svg?v=A00aBgaPR5" color="#b91d47">
+        <link rel="shortcut icon" href="img/favicon.ico?v=A00aBgaPR6">
+        <meta name="apple-mobile-web-app-title" content="NxFITNESS">
+        <meta name="application-name" content="NxFITNESS">
+        <meta name="msapplication-TileColor" content="#db1d1d">
+        <meta name="msapplication-TileImage" content="img/mstile-144x144.png?v=A00aBgaPR5">
+        <meta name="theme-color" content="#ffffff">
--- ux/inc/_html.navBar.php
+++ ux/inc/_html.navBar.php
@@ -1,19 +1,19 @@
 <ul class="nav">
-	<li class="nav-item">
-		<a class="nav-link" href="main.html"><i class="icon-speedometer"></i> Dashboard
-			<span class="badge badge-info">NEW</span></a>
-	</li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="main.html"><i class="icon-speedometer"></i> Dashboard
+                                <span class="badge badge-info">NEW</span></a>
+                        </li>
 
-	<li class="nav-title">
-		UI Elements
-	</li>
-	<li class="nav-item nav-dropdown">
-		<a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Components</a>
-		<ul class="nav-dropdown-items">
-			<li class="nav-item">
-				<a class="nav-link" href="components/buttons.html"><i class="icon-puzzle"></i> Buttons</a>
-			</li>
-		</ul>
-	</li>
+                        <li class="nav-title">
+                            UI Elements
+                        </li>
+                        <li class="nav-item nav-dropdown">
+                            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Components</a>
+                            <ul class="nav-dropdown-items">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="components/buttons.html"><i class="icon-puzzle"></i> Buttons</a>
+                                </li>
+                            </ul>
+                        </li>
 
-</ul>
+                    </ul>
