2aaa01ad4297047e0256bf1cfbeedba6e8cc8a2b
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-15 09:43:55 +0100

    feat(user_ux): Added new boiler plate pages (Feed)
    Added pages for Feed
    
    Issue #73 #88
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ux/inc/_html.navBar.php
+++ ux/inc/_html.navBar.php
@@ -98,5 +98,8 @@
                         <li class="nav-item">
                             <a class="nav-link" href="settings/devices.html"><i class="icon-speedometer"></i> Devices <span class="badge badge-info">NEW</span></a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="settings/feeds.html"><i class="icon-speedometer"></i> Feeds <span class="badge badge-info">NEW</span></a>
+                        </li>
 
                     </ul>
