7154269d9c5dfc918362a781d61e4713a2cded0e
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-16 11:04:53 +0100

    feat(user_ux): Tweeked style
    Closes #100
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ux/js/views/nomie.js
+++ ux/js/views/nomie.js
@@ -7,24 +7,24 @@ $(function () {
         var dbTrackers = data.results.dbTrackers;
 
         $.each(dbTrackers, function (trackerId, dbTracker) {
-            html += '<div class="col-6 col-lg-4" id="'+trackerId+'">';
+            html += '<div class="col-12 col-md-6 col-lg-4" id="'+trackerId+'">';
             html += '    <div class="card">';
             html += '        <div class="card-block p-3 clearfix" style="background-color: '+dbTracker.color+' !important; ">';
             html += '            <div class="row">';
-            html += '                <div class="col-md-3">';
+            html += '                <div class="hidden-sm-down col-md-3">';
             html += '                    <i class="'+dbTracker.icon+' p-3 mr-3 float-left" style="color: #ffffff !important; font-size: 50px;line-height: 50px;"></i>';
             html += '                </div>';
-            html += '                <div class="col-md-9">';
+            html += '                <div class="col-12 col-md-9">';
             html += '                    <div class="row">';
-            html += '                        <div class="col-md-12">';
+            html += '                        <div class="col-12 col-md-12">';
             html += '                            <div class="h5 mb-0 mt-2" style="color: #ffffff;">'+dbTracker.label+'</div>';
             html += '                        </div>';
             html += '                    </div>';
             html += '                    <div class="row">';
-            html += '                        <div class="col-md-6">';
+            html += '                        <div class="col-6 col-md-6">';
             html += '                            <div class="text-muted text-uppercase font-weight-bold font-xs" style="color: #ffffff !important;">'+dbTracker.stats.events+' Events</div>';
             html += '                        </div>';
-            html += '                        <div class="col-md-6">';
+            html += '                        <div class="col-6 col-md-6">';
 
             if (dbTracker.charge < -1) {
                 html += '                            <div class="text-muted text-uppercase font-weight-bold font-xs" style="color: #ffffff !important;">'+dbTracker.charge+' points lost</div>';
@@ -39,10 +39,10 @@ $(function () {
             html += '                        </div>';
             html += '                    </div>';
             html += '                    <div class="row">';
-            html += '                        <div class="col-md-6">';
+            html += '                        <div class="col-6 col-md-6">';
             html += '                            <div class="text-muted text-uppercase font-weight-bold font-xs" style="color: #ffffff !important;">Daily Avg : '+dbTracker.stats.dayAvg+'<br />Monthly Avg : '+dbTracker.stats.monthAvg+'</div>';
             html += '                        </div>';
-            html += '                        <div class="col-md-6">';
+            html += '                        <div class="col-6 col-md-6">';
             html += '                            <div class="text-muted text-uppercase font-weight-bold font-xs" style="color: #ffffff !important;">First : '+dbTracker.stats.first+'<br />Last : '+dbTracker.stats.last+'</div>';
             html += '                        </div>';
             html += '                    </div>';
