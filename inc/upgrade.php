98b2b241663c8b1ef3e17c90dfbb8a6468865dd5
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-20 16:04:20 +0100

    refactor(upgrade): Updated version number when no update functions required

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -650,6 +650,11 @@
 			$this->UpdateFunctions = $updateFunctions;
 			$this->NumUpdates      = count( $updateFunctions );
 
+			if ( $this->NumUpdates == 0 && $currentNumber != $installNumber ) {
+				nxr( "No pending updates, but missmatch version numbers" );
+				$this->setSetting( "version", $installVersion[0] . "." . $installVersion[1] . "." . $installVersion[2] . "." . $installVersion[3], TRUE );
+			}
+
 			return $updateFunctions;
 		}
 
