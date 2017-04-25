eed6f216392b7ce01d2dd74aa2f2340b21661fe9
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-25 21:20:52 +0100

    chore(ci): Added Gitlab CI config

---
+++ ci/docker_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+# Install phpunit, the tool that we will use for testing
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+# Install mysql driver
+# Here you can install any other extension that you need
+docker-php-ext-install pdo_mysql
+
+wget https://phar.phpunit.de/phploc.phar
+chmod +x phploc.phar
+mv phploc.phar /usr/local/bin/phploc
+
+# Install composer dependencies
+wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php'); unlink('installer.sig');"
+mv composer.phar /usr/local/bin/composer.phar
