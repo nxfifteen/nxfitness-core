e8cf0811221f65118ccea500f8db36c89209a9b1
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-27 20:37:48 +0100

    chore(ci): Added build env script

---
+++ dev/build_dev_env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ ! -f "composer.phar" ]; then
+    echo "downloading composer"
+    curl -sS https://getcomposer.org/installer | php >/dev/null 2>&1
+fi
+
+php composer.phar install --dev >/dev/null 2>&1
+
+if [ ! -d "php-docblock-checker-1.3.4" ]; then
+    echo "downloading php-docblock-checker"
+    wget https://github.com/Block8/php-docblock-checker/archive/1.3.4.zip >/dev/null 2>&1
+    unzip 1.3.4.zip >/dev/null 2>&1
+    rm 1.3.4.zip
+    cd php-docblock-checker-1.3.4/
+    php ../composer.phar install >/dev/null 2>&1
+    cd ../
+fi
