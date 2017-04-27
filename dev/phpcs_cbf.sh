96d4eff4b4b38fbbaefb0812ab3729fa8ee9490c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-27 22:32:33 +0100

    chore(ci): Silence fixer

--- dev/phpcs_cbf.sh
+++ dev/phpcs_cbf.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-./vendor/bin/phpcbf --standard="PSR2" --ignore="vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./
+echo -n "Running format fixer..."
+./vendor/bin/phpcbf --standard="PSR2" --ignore="vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./ >/dev/null 2>&1
+echo "[DONE]"
+
+echo -n "Running syntax checks..."
 ./vendor/bin/phpcs --standard="PSR2" --ignore="vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./
 return $?
