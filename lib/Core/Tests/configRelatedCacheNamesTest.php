0f1392b4355ee841402ca13b9f449b2c1d49281b
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-30 23:30:21 +0100

    style(code analysis): Loading classes into test script from autoloader

--- lib/Core/Tests/configRelatedCacheNamesTest.php
+++ lib/Core/Tests/configRelatedCacheNamesTest.php
@@ -2,7 +2,7 @@
 
     namespace Core\Tests;
 
-    require_once(dirname(__FILE__) . '/../inc/Config.php');
+    require_once(dirname(__FILE__) . '/../../autoloader.php');
 
     use Core\Config as Config;
     use \PHPUnit\Framework\TestCase;
