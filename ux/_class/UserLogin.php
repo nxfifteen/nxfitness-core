8a92ef16e1e51173c9fe11b3cc8f8345ab925866
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-04-30 08:56:32 +0100

    fix(namespace): Imported core namespace

--- ux/_class/UserLogin.php
+++ ux/_class/UserLogin.php
@@ -1,4 +1,6 @@
 <?php
+    use Core\Core;
+
     header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
     header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
     header('Cache-Control: no-store, no-cache, must-revalidate');
