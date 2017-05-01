cf3b9b2d99b66e0ff579db3c9c4a9d2bfe147210
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-05-01 22:18:44 +0100

    chore(version_number): Bumped 0.0.1.5 to 0.0.1.6

--- config.def.dist.php
+++ config.def.dist.php
@@ -1,5 +1,5 @@
 <?php
-    define("APP_VERSION", "0.0.1.5");
+    define("APP_VERSION", "0.0.1.6");
     //define( 'SENTRY_DSN', 'https://80a480ea986d4ee993ac89a54a0d1f0e@sentry.io/156527' );
     if (file_exists(dirname(__FILE__) . "/config.def.php")) {
         require_once(dirname(__FILE__) . "/config.def.php");
