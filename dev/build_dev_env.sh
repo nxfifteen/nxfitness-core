59d5f47d18a85385f5871bbb80ba62fa769cb239
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-05-20 08:52:11 +0100

    chore(ci): Updated tests

--- dev/build_dev_env.sh
+++ dev/build_dev_env.sh
@@ -5,12 +5,12 @@ WORKINGDIR=`pwd`
 echo "# Working in $WORKINGDIR"
 
 echo "## Updated System Image"
-apt-get update -yqq
-apt-get install -yqq git 
-apt-get install -yqq zip 
-apt-get install -yqq unzip 
-apt-get install -yqq wget 
-apt-get install -yqq php-xdebug
+apt-get update -yqq >/dev/null 2>&1
+apt-get install -yqq git >/dev/null 2>&1
+apt-get install -yqq zip >/dev/null 2>&1
+apt-get install -yqq unzip >/dev/null 2>&1
+apt-get install -yqq wget >/dev/null 2>&1
+apt-get install -yqq php-xdebug >/dev/null 2>&1
 
 if [ ! -d "binaries" ]; then
     echo "## Creating Binaries Folder"
@@ -31,9 +31,10 @@ php "$WORKINGDIR/binaries/composer.phar" install --dev >/dev/null 2>&1
 if [ ! -d "php-docblock-checker-1.3.4" ]; then
     echo "### Downloading php-docblock-checker"
     cd "$WORKINGDIR/binaries"
-    wget https://github.com/Block8/php-docblock-checker/archive/1.3.4.zip -O "$WORKINGDIR/binaries/1.3.4.zip"
+    wget https://github.com/Block8/php-docblock-checker/archive/1.3.4.zip -O "$WORKINGDIR/binaries/1.3.4.zip" >/dev/null 2>&1
     unzip "$WORKINGDIR/binaries/1.3.4.zip" >/dev/null 2>&1
     rm "$WORKINGDIR/binaries/1.3.4.zip"
+    ls -al "$WORKINGDIR/binaries"
     cd "$WORKINGDIR/php-docblock-checker-1.3.4"
     php "$WORKINGDIR/binaries/composer.phar" install #>/dev/null 2>&1
     cd "$WORKINGDIR/"
