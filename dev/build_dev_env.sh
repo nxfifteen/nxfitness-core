bc64358de3c4788de49fc1a6f5acc7f2ed09c51a
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-05-20 09:05:36 +0100

    chore(ci): Updated tests

--- dev/build_dev_env.sh
+++ dev/build_dev_env.sh
@@ -35,7 +35,7 @@ if [ ! -d "php-docblock-checker-1.3.4" ]; then
     unzip "$WORKINGDIR/binaries/1.3.4.zip" >/dev/null 2>&1
     rm "$WORKINGDIR/binaries/1.3.4.zip"
     cd "$WORKINGDIR/binaries/php-docblock-checker-1.3.4"
-    php "$WORKINGDIR/binaries/composer.phar" install #>/dev/null 2>&1
+    php "$WORKINGDIR/binaries/composer.phar" install >/dev/null 2>&1
     cd "$WORKINGDIR/"
 fi
 
