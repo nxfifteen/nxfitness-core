00c132fe086d1758558f8ea8eeaa2046c05222d0
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-05-20 11:35:37 +0100

    chore(ci): Updated tests

--- dev/build_dev_env.sh
+++ dev/build_dev_env.sh
@@ -27,21 +27,3 @@ fi
 echo "#### Installing Composer DEV"
 cp composer_dev.json composer.json
 php "$WORKINGDIR/binaries/composer.phar" install --dev >/dev/null 2>&1
-
-if [ ! -d "php-docblock-checker-1.3.4" ]; then
-    echo "### Downloading php-docblock-checker"
-    cd "$WORKINGDIR/binaries"
-    wget https://github.com/Block8/php-docblock-checker/archive/1.3.4.zip -O "$WORKINGDIR/binaries/1.3.4.zip" >/dev/null 2>&1
-    unzip "$WORKINGDIR/binaries/1.3.4.zip" >/dev/null 2>&1
-    rm "$WORKINGDIR/binaries/1.3.4.zip"
-    cd "$WORKINGDIR/binaries/php-docblock-checker-1.3.4"
-    php "$WORKINGDIR/binaries/composer.phar" install >/dev/null 2>&1
-    cd "$WORKINGDIR/"
-fi
-
-if [ ! -f "$WORKINGDIR/binaries/phpunit.phar" ]; then
-    echo "### Downloading phpunit"
-    cd "$WORKINGDIR/binaries"
-    wget https://phar.phpunit.de/phpunit.phar >/dev/null 2>&1
-    cd "$WORKINGDIR/"
-fi
