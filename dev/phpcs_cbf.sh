3f5caab5fe18795bef8962f38b8ce4b8492ff60e
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-05-21 11:32:38 +0100

    chore(ci): Corrected Paths
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- dev/phpcs_cbf.sh
+++ dev/phpcs_cbf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo -n "Running format fixer..."
-./vendor/bin/phpcbf --standard="PSR2" --ignore="bundle,vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./ >/dev/null 2>&1
+./bundle/vendor/bin/phpcbf --standard="PSR2" --ignore="bundle,vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./ >/dev/null 2>&1
 echo "[DONE]"
 
 echo -n "Running syntax checks..."
-./vendor/bin/phpcs --standard="PSR2" --ignore="bundle,vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./
+./bundle/vendor/bin/phpcs --standard="PSR2" --ignore="bundle,vendor,library,php-docblock-checker-1.3.4,phpspec" --extensions="php,css,js" --error-severity=1 --warning-severity=8 --exclude=Generic.WhiteSpace.ScopeIndent ./
 if [ $? == 0 ]; then
     echo "[DONE]"
     exit 0
