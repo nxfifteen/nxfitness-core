1e1a093967ad4d4b44181b41ce68633aa87ce2ba
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-01 18:39:50 +0100

    fix(dev): Updated upgrade function translator
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- dev/updateVer.php
+++ dev/updateVer.php
@@ -8,22 +8,6 @@
  * file that was distributed with this source code.
  ******************************************************************************/
 
-/**
- * Core - Update commandline tool
- *
- * @version   0.0.1
- * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
- * @link      http://nxfifteen.me.uk NxFIFTEEN
- * @copyright 2015 Stuart McCulloch Anderson
- * @license   http://stuart.nx15.at/mit/2015 MIT
- */
-
-parse_str( implode( '&', array_slice( $argv, 1 ) ), $argv );
-foreach ( $argv as $key => $value ) {
-    $key          = str_ireplace( "--", "", $key );
-    $_GET[ $key ] = $value;
-}
-
-echo $_GET[ 'ver' ] . " => ";
-$_GET[ 'ver' ] = explode( ".", $_GET[ 'ver' ] );
-echo ( ( ( $_GET[ 'ver' ][ 0 ] * 1000 ) . ( $_GET[ 'ver' ][ 1 ] * 100 ) . ( $_GET[ 'ver' ][ 2 ] * 10 ) . $_GET[ 'ver' ][ 3 ] ) * 1 );
+echo $argv[1] . " => ";
+$argv[1] = explode( ".", $argv[1] );
+echo ( ( ( $argv[1][ 0 ] * 1000 ) . ( $argv[1][ 1 ] * 100 ) . ( $argv[1][ 2 ] * 10 ) . $argv[1][ 3 ] ) * 1 );
