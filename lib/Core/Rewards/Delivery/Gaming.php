591d5bf29fa3f3f7dc03d2583893b7f92405779f
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-05 21:57:55 +0100

    fix(gaming): Updated gaming system to use new balancing table filtered on class and skill
    Issue #206
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- lib/Core/Rewards/Delivery/Gaming.php
+++ lib/Core/Rewards/Delivery/Gaming.php
@@ -25,27 +25,85 @@ require_once(dirname(__FILE__) . "/../../../autoloader.php");
  */
 class Gaming extends Delivery
 {
+    private $blancing = [];
+    
     /**
-     * @param int $xp
+     * @param array $rewardProfile
      * @param string $state
      * @param string $rewardKey
      */
-    public function deliver($xp, $state, $rewardKey)
+    public function deliver($rewardProfile, $state, $rewardKey)
     {
-        $db_prefix = $this->getAppClass()->getSetting("db_prefix", null, false);
-        if (!$this->getAppClass()->getDatabase()->has($db_prefix . "users_xp", ['fuid' => $this->getUserID()])) {
-            $this->getAppClass()->getDatabase()->insert($db_prefix . "users_xp", ["xp" => 0, "fuid" => $this->getUserID()]);
-            $dbCurrentXp = 0;
+        if (!$this->getAppClass()->getDatabase()->has($this->dbPrefix . "users_xp", ['fuid' => $this->getUserID()])) {
+            $this->getAppClass()->getDatabase()->insert($this->dbPrefix . "users_xp", ["class" => "Rebel", "xp" => 1, "mana" => 1, "health" => 1, "fuid" => $this->getUserID()]);
+            $dbCurrent = ["class" => "Rebel", "xp" => 1, "mana" => 1, "health" => 1];
         } else {
-            $dbCurrentXp = $this->getAppClass()->getDatabase()->get($db_prefix . "users_xp", 'xp', ["fuid" => $this->getUserID()]);
+            $dbCurrent = $this->getAppClass()->getDatabase()->get($this->dbPrefix . "users_xp", ['class','xp','mana','health'], ["fuid" => $this->getUserID()]);
         }
 
-        $newXp = $dbCurrentXp + $xp;
-        if ($newXp < 0) $newXp = 0;
+        $balancingRules = $this->get(["class" => $dbCurrent['class'], "skill" => $rewardProfile['skill']], ["xp" => 1, "mana" => 1, "health" => 1]);
 
-        $this->getAppClass()->getDatabase()->update($db_prefix . "users_xp", ["xp" => $newXp], ["fuid" => $this->getUserID()]);
+        $updatedValues = [];
+        if (array_key_exists("xp", $rewardProfile)) {
+            $updatedValues['xp'] = round($dbCurrent['xp'] + ($rewardProfile['xp'] * $balancingRules['xp']), 0);
+            if ($updatedValues['xp'] < 0) $updatedValues['xp'] = 0;
+        }
+        if (array_key_exists("mana", $rewardProfile)) {
+            $updatedValues['mana'] = round($dbCurrent['mana'] + ($rewardProfile['mana'] * $balancingRules['mana']), 0);
+            if ($updatedValues['mana'] < 0) $updatedValues['mana'] = 0;
+        }
+        if (array_key_exists("health", $rewardProfile)) {
+            $updatedValues['health'] = round($dbCurrent['health'] + ($rewardProfile['health'] * $balancingRules['health']), 0);
+            if ($updatedValues['health'] < 0) $updatedValues['health'] = 0;
+        }
+
+        $this->getAppClass()->getDatabase()->update($this->dbPrefix . "users_xp", $updatedValues, ["fuid" => $this->getUserID()]);
         $this->getAppClass()->getErrorRecording()->postDatabaseQuery($this->getAppClass()->getDatabase(), ["METHOD" => __METHOD__, "LINE" => __LINE__]);
 
         $this->recordDevlivery([], "delivered", $rewardKey);
+    }
+
+    /**
+     * Return setting value
+     * Main function called to query settings for value. Default value can be provided, if not NULL is returned.
+     * Values can be queried in the database or limited to config file and 'live' values
+     *
+     * @param array $key Setting to query
+     * @param array $default Default value to return
+     * @param bool $query_db Boolean to search database or not
+     *
+     * @return array Setting value, or default as per defined
+     */
+    public function get($key, $default = null, $query_db = true)
+    {
+        if ($query_db && $this->getAppClass()->getDatabase()->has($this->dbPrefix . "blancing", $key)) {
+            $dbResults = $this->getAppClass()->getDatabase()->get($this->dbPrefix . "blancing", ['xp','mana','health'], $key);
+            return $dbResults;
+        } else {
+            if ($query_db && !is_null($default)) {
+                $this->set($key, $default);
+            }
+
+            return $default;
+        }
+    }
+
+    /**
+     * Set setting value
+     * Function to store/change setting values. Values can be stored in the database or held in memory.
+     *
+     * @param array $key Setting to query
+     * @param array $value Value to store
+     * @param bool $query_db Boolean to store in database or not
+     *
+     * @return bool was data stored correctly
+     */
+    public function set($key, $value)
+    {
+        if ($this->getAppClass()->getDatabase()->has($this->dbPrefix . "blancing", $key)) {
+            $this->getAppClass()->getDatabase()->update($this->dbPrefix . "blancing", $value, $key);
+        } else {
+            $this->getAppClass()->getDatabase()->insert($this->dbPrefix . "blancing", array_merge($value, $key));
+        }
     }
 }
