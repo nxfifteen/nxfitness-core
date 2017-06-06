12d3570270735de5641b889fea49d10546e0aac4
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-06 18:54:32 +0100

    fix(gaming): Added user level and precentage to next level columns
    Issue #206
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- lib/Core/Deploy/Upgrade.php
+++ lib/Core/Deploy/Upgrade.php
@@ -971,4 +971,19 @@ class Upgrade
         return true;
     }
 
+    /** @noinspection PhpUnusedPrivateMethodInspection */
+    private function updateRun1011()
+    {
+        $db_prefix = $this->getSetting("db_prefix", false);
+
+        $this->getDatabase()->query("ALTER TABLE `" . $db_prefix . "users_xp` ADD `level` INT(3) NOT NULL DEFAULT '0' AFTER `xp`, ADD `percent` INT(3) NOT NULL DEFAULT '0' AFTER `level`;");
+        if ($this->wasMySQLError($this->getDatabase()->error())) {
+            return false;
+        }
+
+        $this->setSetting("version", "0.0.1.11", true);
+
+        return true;
+    }
+
 }
