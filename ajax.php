602db5cfc5a8d5490a44bef79d82dc40c74e469e
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-18 11:22:26 +0100

    feat(acc_del): Updated AJAX to identify forms
    Issue #220
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ajax.php
+++ ajax.php
@@ -19,66 +19,81 @@ require_once( dirname( __FILE__ ) . "/lib/autoloader.php" );
 if (array_key_exists('_nx_fb_usr', $_COOKIE) && $_COOKIE['_nx_fb_usr'] != "") {
     nxr(1, "User " . $_COOKIE['_nx_fb_usr'] . " Secured");
 
-    if (array_key_exists('token', $_POST) && array_key_exists('userid', $_POST)) {
-        $apiUserid = $_POST['userid'];
-        $apiKey = $_POST['token'];
+    if (array_key_exists('formId', $_POST)) {
+        if ($_POST['formId'] == "habiticaRegister" || $_POST['formId'] == "habiticaConnect") {
+            if (array_key_exists('token', $_POST) && array_key_exists('userid', $_POST)) {
+                $apiUserid = $_POST['userid'];
+                $apiKey = $_POST['token'];
 
-        $habiticaClass = new HabitRPHPG($apiUserid, $apiKey);
-        $newUser = $habiticaClass->_request("get", "user", '', true);
-        if (!$newUser['_id']) {
-            http_response_code(500);
-            if ($newUser['message'] == "There is no account that uses those credentials.") {
-                echo "There is a problem with your credentials, please check the user id and api key before trying again. Or create an account bellow.";
+                $habiticaClass = new HabitRPHPG($apiUserid, $apiKey);
+                $newUser = $habiticaClass->_request("get", "user", '', true);
+                if (!$newUser['_id']) {
+                    http_response_code(500);
+                    if ($newUser['message'] == "There is no account that uses those credentials.") {
+                        echo "There is a problem with your credentials, please check the user id and api key before trying again. Or create an account bellow.";
+                    } else {
+                        nxr(1, print_r($newUser, true));
+                        echo "Unknown error: " . $newUser['error'] . " :: " . $newUser['message'];
+                    }
+
+                    die();
+                }
+
             } else {
-                nxr(1, print_r($newUser, true));
-                echo "Unknown error: " . $newUser['error'] . " :: " . $newUser['message'];
+                $_POST['confirmPassword'] = $_POST['password2'];
+                unset($_POST['password2']);
+                $habiticaClass = new HabitRPHPG(null, null);
+                $newUser = $habiticaClass->_request("post", "user/auth/local/register", $_POST, true);
+
+                if (!$newUser['_id']) {
+                    http_response_code(500);
+                    if ($newUser['message'] == "Username already taken.") {
+                        echo "Username already taken. If this is your account please use the form above to connect to it.";
+                    } else if ($newUser['message'] == "Email address is already used in an account.") {
+                        echo "Email address is already used in an account. If you already have an account please use the form above to connect to it.";
+                    } else if ($newUser['message'] == "Invalid request parameters.") {
+                        foreach ($newUser['errors'] as $error) {
+                            echo $error['message'] . "<br />";
+                        }
+                    } else {
+                        nxr(1, print_r($newUser, true));
+                        echo "Unknown error: " . $newUser['error'] . " :: " . $newUser['message'];
+                    }
+
+                    die();
+                }
+
+                $apiUserid = $newUser['id'];
+                $apiKey = $newUser['apiToken'];
             }
 
-            die();
+            $core = new Core();
+            if (isset($apiUserid) && isset($apiKey)) {
+                $core->setUserSetting($_COOKIE['_nx_fb_usr'], "user_id", $apiUserid);
+                $core->setUserSetting($_COOKIE['_nx_fb_usr'], "api_key", $apiKey);
+            }
+            $core->getFitbitAPI( $_COOKIE['_nx_fb_usr'] )->setForceSync( true );
+            $core->getFitbitAPI( $_COOKIE['_nx_fb_usr'] )->pull( $_COOKIE['_nx_fb_usr'], 'habitica' );
+
+            $cacheFile = 'cache' . DIRECTORY_SEPARATOR . '_' . $_COOKIE[ '_nx_fb_usr' ] . '_xp';
+            if ( file_exists( $cacheFile ) ) {
+                unlink($cacheFile);
+            }
+
+            http_response_code(200);
+        } else if ($_POST['formId'] == "") {
+
+        } else {
+            nxr(1, "Unknown Form");
+            http_response_code(403);
+            echo "Unknown Form";
         }
 
     } else {
-        $_POST['confirmPassword'] = $_POST['password2'];
-        unset($_POST['password2']);
-        $habiticaClass = new HabitRPHPG(null, null);
-        $newUser = $habiticaClass->_request("post", "user/auth/local/register", $_POST, true);
-
-        if (!$newUser['_id']) {
-            http_response_code(500);
-            if ($newUser['message'] == "Username already taken.") {
-                echo "Username already taken. If this is your account please use the form above to connect to it.";
-            } else if ($newUser['message'] == "Email address is already used in an account.") {
-                echo "Email address is already used in an account. If you already have an account please use the form above to connect to it.";
-            } else if ($newUser['message'] == "Invalid request parameters.") {
-                foreach ($newUser['errors'] as $error) {
-                    echo $error['message'] . "<br />";
-                }
-            } else {
-                nxr(1, print_r($newUser, true));
-                echo "Unknown error: " . $newUser['error'] . " :: " . $newUser['message'];
-            }
-
-            die();
-        }
-
-        $apiUserid = $newUser['id'];
-        $apiKey = $newUser['apiToken'];
+        nxr(1, "Invalid Form");
+        http_response_code(403);
+        echo "Invalid Form";
     }
-
-    $core = new Core();
-    if (isset($apiUserid) && isset($apiKey)) {
-        $core->setUserSetting($_COOKIE['_nx_fb_usr'], "user_id", $apiUserid);
-        $core->setUserSetting($_COOKIE['_nx_fb_usr'], "api_key", $apiKey);
-    }
-    $core->getFitbitAPI( $_COOKIE['_nx_fb_usr'] )->setForceSync( true );
-    $core->getFitbitAPI( $_COOKIE['_nx_fb_usr'] )->pull( $_COOKIE['_nx_fb_usr'], 'habitica' );
-
-    $cacheFile = 'cache' . DIRECTORY_SEPARATOR . '_' . $_COOKIE[ '_nx_fb_usr' ] . '_xp';
-    if ( file_exists( $cacheFile ) ) {
-        unlink($cacheFile);
-    }
-
-    http_response_code(200);
 
 } else {
     nxr(1, "User Unsecured");
