3144f6e41fd05f887fe7eb8052b9addcd25baaa1
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-25 09:34:07 +0100

    feat(nxr):Removed unrequired print_r's
    Closed #267
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- lib/Core/Babel/ApiBabel.php
+++ lib/Core/Babel/ApiBabel.php
@@ -1,23 +1,12 @@
 <?php
-/**
+/*******************************************************************************
  * This file is part of NxFIFTEEN Fitness Core.
  *
  * Copyright (c) 2017. Stuart McCulloch Anderson
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @package     Core
- * @subpackage  Babel
- * @version     0.0.1.x
- * @since       0.0.0.1
- * @author      Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
- * @link        https://nxfifteen.me.uk NxFIFTEEN
- * @link        https://nxfifteen.me.uk/nxcore Project Page
- * @link        https://nxfifteen.me.uk/gitlab/nx-fitness/nxfitness-core Git Repo
- * @copyright   2017 Stuart McCulloch Anderson
- * @license     https://nxfifteen.me.uk/api/license/mit/2015-2017 MIT
- */
+ ******************************************************************************/
 
 namespace Core\Babel;
 
@@ -472,7 +461,7 @@ class ApiBabel
                     } catch (Exception $e) {
                         nxr(4, $nomie_url);
                         $parts = parse_url($nomie_url);
-                        nxr(0, print_r($parts, true));
+                        nxr(0, $parts);
 
                         if (!isset($nomie_username)) {
                             $was_username_set = "false";
@@ -1055,8 +1044,8 @@ class ApiBabel
             }
 
             if ($debugOutput) {
-                nxr(0, print_r($request, true));
-                nxr(0, print_r($response, true));
+                nxr(0, $request);
+                nxr(0, $response);
             }
 
             return $response;
