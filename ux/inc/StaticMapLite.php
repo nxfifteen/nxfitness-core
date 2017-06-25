d2f576517191a5dbed50f15b3ed77b10dbbe4b6f
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-06-25 09:59:48 +0100

    refactor(docblock): Put back blocks, removed by IDE
    Issue #265
    
    Signed-off-by: Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>

--- ux/inc/StaticMapLite.php
+++ ux/inc/StaticMapLite.php
@@ -1,12 +1,21 @@
 <?php
-/*******************************************************************************
- * This file is part of NxFIFTEEN Fitness Core.
+/**
+ * StaticMapLite 0.3.1
+ * Copyright 2009 Gerhard Koch
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Copyright (c) 2017. Stuart McCulloch Anderson
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- ******************************************************************************/
+ * @author Gerhard Koch <gerhard.koch AT ymail.com>
+ * USAGE:
+ *  StaticMapLite.php?center=40.714728,-73.998672&zoom=14&size=512x512&maptype=mapnik&markers=40.702147,-74.015794,blues|40.711614,-74.012318,greeng|40.718217,-73.998284,redc
+ */
 
 namespace Map;
 
