627e54d083ac89ada5afaa60facc70ae94c6901c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2017-08-05 12:54:40 +0100

    refactor(json): Updated Access-Control-Allow-Origin

--- ajax.php
+++ ajax.php
@@ -20,7 +20,7 @@
 
 use Core\Core;
 
-header( 'Access-Control-Allow-Origin: http://nxdev.dev.itsabeta.nx' );
+header( 'Access-Control-Allow-Origin: https://nxdev.dev.itsabeta.nx' );
 header( 'Cache-Control: no-cache, must-revalidate' );
 header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
 
